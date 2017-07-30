a73ae799748813963b56a338a39a69917780f511
hsnee <2061022a@student.gla.ac.uk>
2017-07-30 18:29:30 -0400

    cleaner code

--- FpPlot.py
+++ FpPlot.py
@@ -1,19 +1,17 @@
-from angles import r2arcs
+from angles import r2arcs, r2d
 import sys, os, matplotlib
-import lsst.afw.cameraGeom as cameraGeom
-import lsst.afw.geom as geom
+import lsst.afw.geom.Point2D as Point2D
 
 if len(sys.argv)<2:
     sys.stderr.write("Syntax: python test-psf.py  repo_path\n")
-    sys.exit(1)
+    exit()
 
 repo_rel_path = sys.argv[1]
 repo_abs_path = os.path.abspath(repo_rel_path)
 
 if not os.path.exists(repo_abs_path):
     sys.stderr.write("Nothing found at {}\n".format(repo_abs_path))
-    sys.exit(1)
-
+    exit()
 
 import numpy as np
 matplotlib.use("pdf")
@@ -23,21 +21,14 @@ import seaborn as sns;sns.set_style('darkgrid')
 
 # Create a data butler which provides access to a data repository.
 butler = lsst.daf.persistence.Butler(repo_abs_path)
-print "Butler summoned (i.e. we have loaded the data repository)"
-print repo_abs_path
+print "Butler summoned (i.e. we have loaded the data repository)", repo_abs_path
 ccd_exposures = butler.queryMetadata("calexp", format=['visit', 'ccd'])
 ccd_exposures = [(visit,ccd) for (visit,ccd) in ccd_exposures if butler.datasetExists("calexp", visit=visit, ccd=ccd)]
 
 print "Found {} (exposure,ccd) pairs".format(len(ccd_exposures))
 for ccd in range(104):
     if ccd ==1:
-        old_X = X
-        old_Y = Y
-        old_U = U
-        old_C = C
-        old_angles = angles
-    else:
-        pass
+        old_X, Old_Y, old_U, old_C, old_angles = X, Y, U, C, angles
     if ccd==0:
         pass
     else:
@@ -47,11 +38,9 @@ for ccd in range(104):
     calexp = butler.get("calexp", visit=visit, ccd=ccd, immediate=True)
     detector = calexp.getDetector()
     print detector.getName()
-    width = calexp.getWidth()
-    height = calexp.getHeight()
-    psf = calexp.getPsf()
-    wcs = calexp.getWcs()
-    #pixel_scale = wcs.pixelScale() # if using pixel coordinates.
+    width, height, psf, wcs = calexp.getWidth(), calexp.getHeight(), calexp.getPsf(), calexp.getWcs()
+
+    # pixel_scale = wcs.pixelScale() # if using pixel coordinates.
     pixel_scale = 1. # if using FOCAL_PLANE coordinates
     if ccd==0:
         pass
@@ -61,9 +50,10 @@ for ccd in range(104):
     ny = int(nx * (height/width))
     angles = np.zeros((ny,nx))
     X,Y,U,C = (np.zeros((ny,nx)) for _ in range(4))
-    for i,x in enumerate(np.linspace(0, width, nx)):
-        for j,y in enumerate(np.linspace(0, height, ny)):
-            image = psf.computeKernelImage(lsst.afw.geom.Point2D(x, y))
+    for i,x in enumerate(np.linspace(0, width, nx-1)):
+        for j,y in enumerate(np.linspace(0, height, ny-1)):
+            point = Point2D(x,y)
+            image = psf.computeKernelImage(point)
             array = image.getArray()
             galsim_image = galsim.Image(array)
             shape_data = galsim.hsm.FindAdaptiveMom(galsim_image,strict=False)
@@ -72,26 +62,18 @@ for ccd in range(104):
             else:
                 print shape_data.error_message
                 continue
-            FpPos = wcs.pixelToSky(lsst.afw.geom.Point2D(x,y))
-            X[j,i] = FpPos.getLongitude()
-            Y[j,i] = FpPos.getLatitude()
-            e1 = shape_data.observed_shape.e1
-            e2 = shape_data.observed_shape.e2
+            FpPos = wcs.pixelToSky(point)
+            X[j,i], Y[j,i] = FpPos.getLongitude(), FpPos.getLatitude()
+            e1, e2 = shape_data.observed_shape.e1, shape_data.observed_shape.e2
             U[j,i] = np.sqrt(e1**2+e2**2)
             C[j,i] = shape_data.moments_sigma
             if C[j,i]<0:
                 print 'negative size detected :/'
-            else:
-                pass
-            angles[j,i] = 0.5*np.arctan2(e2,e1)*180/np.pi
+            angles[j,i] = angles.r2d(0.5*np.arctan2(e2,e1))
     if ccd == 0:
         pass
     else:
-        old_X = np.append(X,old_X)
-        old_Y = np.append(Y,old_Y)
-        old_U = np.append(U,old_U)
-        old_C = np.append(C,old_C)
-        old_angles = np.append(angles, old_angles)
+        old_X, old_Y, old_U, old_C, old_angles = np.append(X,old_X), np.append(Y,old_Y), np.append(U,old_U), np.append(C,old_C), np.append(angles, old_angles)
 
 # Plotting
 std2fwhm = 2.*np.sqrt(2.*np.log(2.))
@@ -100,8 +82,7 @@ pixel_scale = r2arcs(pixel_scale)
 
 plt.figure()
 Q = plt.quiver(pixel_scale*(old_X-np.median(old_X)),pixel_scale*(old_Y-np.median(old_Y)),old_U,old_V,std2fwhm*old_C,angles=old_angles,headlength=0,headaxislength=0,scale=20,cmap='viridis')
-qk = plt.quiverkey(Q, 0.9, 0.9, 0.05, r'$|e|=0.05$', labelpos='E',
-                   coordinates='figure')
+qk = plt.quiverkey(Q, 0.9, 0.9, 0.05, r'$|e|=0.05$', labelpos='E', coordinates='figure')
 plt.title("whisker plot")
 plt.colorbar(label='FWHM')
 #plt.axis('equal')
