f0a0f8eee555d95d0e7e28f87e4bace653aee129
hsnee <husni@physics.org>
2017-08-12 18:43:14 -0700

    debugged

--- correlation_tests.py
+++ correlation_tests.py
@@ -1,10 +1,12 @@
 import numpy as np
+import matplotlib
+matplotlib.use("pdf")
 import matplotlib.pyplot as plt
 import seaborn as sns;sns.set_style('darkgrid')
 
-data0 = np.loadtxt('0\ corr_arrays.txt')
-data1 = np.loadtxt('1\ corr_arrays.txt')
-data2 = np.loadtxt('2\ corr_arrays.txt')
+data0 = np.loadtxt('0 corr_arrays.txt')
+data1 = np.loadtxt('1 corr_arrays.txt')
+data2 = np.loadtxt('2 corr_arrays.txt')
 ra0,dec0,g10,g20 = data0[0],data0[1],data0[2],data0[3]
 ra1,dec1,g11,g21 = data1[0],data1[1],data1[2],data1[3]
 ra2,dec2,g12,g22 = data2[0],data2[1],data2[2],data2[3]
