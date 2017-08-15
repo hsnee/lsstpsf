c78235dea1d4d1e352bac60ce290af3d6093793d
Husni Almoubayyed <husni@physics.org>
2017-08-15 17:33:24 -0400

    fractional delta shear correlation function

--- correlation_tests.py
+++ correlation_tests.py
@@ -10,11 +10,14 @@ data2 = np.loadtxt('2 corr_arrays.txt')
 ra0,dec0,g10,g20 = data0[0],data0[1],data0[2],data0[3]
 ra1,dec1,g11,g21 = data1[0],data1[1],data1[2],data1[3]
 ra2,dec2,g12,g22 = data2[0],data2[1],data2[2],data2[3]
-
+g1_avg = 0.5*(g10 + g11)
+g2_avg = 0.5*(g20 + g21)
+delta_g1 = g11-g10
+delta_g2 = g21-g20
 # Now instead of plotting things let's input them into treecorr and get a gamma-gamma correlation function.
 import treecorr
 
-cat = treecorr.Catalog(g1=g11-g10, g2=g21-g20, ra=ra1, dec=dec1,ra_units='radians',dec_units='radians')
+cat = treecorr.Catalog(g1=delta_g1/g1_avg, g2=delta_g2/g2_avg, ra=ra1, dec=dec1,ra_units='radians',dec_units='radians')
 gg = treecorr.GGCorrelation(min_sep=1, max_sep=200, nbins=40, sep_units='arcmin')
 gg.process(cat)
 xip = gg.xip
@@ -39,5 +42,5 @@ plt.ylabel(r'$\xi$')
 plt.xscale('log')
 plt.yscale('log')
 plt.legend([r'$\xi_+$',r'$\xi_-$'],bbox_to_anchor=(1, 1), loc='upper right', borderaxespad=0.)
-plt.savefig('delta_shear.pdf')
+plt.savefig('fractional_delta_shear.pdf')
 plt.close()
