f73636778093caf008f861c3a078464bf635e847
hsnee <husni@physics.org>
2017-08-16 18:29:56 -0700

    result from last commit

--- divergingtest.py
+++ divergingtest.py
@@ -15,7 +15,7 @@ g2 = r*np.sin(2*theta)
 # Now instead of plotting things let's input them into treecorr and get a gamma-gamma correlation function.
 import treecorr
 
-cat = treecorr.Catalog(g1=g1, g2=g2, ra=ra1, dec=dec1,ra_units='radians',dec_units='radians')
+cat = treecorr.Catalog(g1=g1, g2=g2, ra=ra, dec=dec, ra_units='arcsec',dec_units='arcsec')
 gg = treecorr.GGCorrelation(min_sep=1, max_sep=200, nbins=40, sep_units='arcmin')
 gg.process(cat)
 xip = gg.xip
@@ -23,4 +23,22 @@ xim = gg.xim
 sigma = gg.varxi**0.5
 
 # plot the correlation functions:
+import seaborn.timeseries
+
+def _plot_std_bars(std=None, central_data=None, ci=None, data=None,*args, **kwargs):
+    std = sigma
+    ci = np.asarray((central_data - std, central_data + std))
+    kwargs.update({"central_data": central_data, "ci": ci, "data": data})
+    seaborn.timeseries._plot_ci_band(*args, **kwargs)
+seaborn.timeseries._plot_std_bars = _plot_std_bars
 r = np.exp(gg.meanlogr)
+plt.figure()
+sns.tsplot(xip,r,err_style='std_bars')
+sns.tsplot(xim,r,err_style='std_bars',color='r')                                                       
+plt.xlabel(r'$\theta$ (arcmin)')
+plt.ylabel(r'$\xi$')
+plt.xscale('log')
+plt.yscale('log')
+plt.legend([r'$\xi$'],bbox_to_anchor=(1, 1), loc='upper right', borderaxespad=0.)
+plt.savefig('diverging_test.pdf')
+plt.close()
