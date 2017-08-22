8acd4fca4eb5b79ed1275bf9ae718a68134e4176
Husni Almoubayyed <husni@physics.org>
2017-08-22 14:43:44 -0400

    pcolormesh -> pcolor

--- plotting.py
+++ plotting.py
@@ -76,7 +76,7 @@ def HSC_style_plots(X,Y,e1a,e2a,e1b,e2b,sigmaa,sigmab):
         X[i],Y[i] = np.array(map(lambda x: angles.r2arcs(x), (X[i],Y[i])))
     print X,Y,e1a-e1b
     plt.figure(1)
-    plt.pcolormesh(X,Y,e1a-e1b,cmap='viridis')
+    plt.pcolor(X,Y,e1a-e1b,cmap='viridis')
     plt.colorbar()
     plt.xlabel('arcsec')
     plt.ylabel('arcsec')
